783520d1ec609b7073612fbf478ad744a87ef230
Iyur <iyurrr@gmail.com>
2024-04-22 15:08:27 +0300

    File test

--- api/test.py
+++ api/test.py
@@ -1,13 +1,21 @@
+import json
+from datetime import datetime
 from http.server import BaseHTTPRequestHandler
- 
+
+def test():
+    data = {
+        "test": True
+    }
+    with open("\\data\\" + datetime.now().strftime("%m-%d") + ".json", "w") as file:
+        json.dump(data, file)
+
+
 class handler(BaseHTTPRequestHandler):
  
     def do_GET(self):
+        test()
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
         self.wfile.write('Just a test'.encode('utf-8'))
         return
-
-
-print("qwerty")
